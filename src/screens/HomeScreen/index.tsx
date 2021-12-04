8e4cec2bb7c5a47ce0f6bee8c0b9005bef904839
sas10x <kristanraearcino@gmail.com>
2021-12-04 18:11:10 +0800

    initial

---
+++ src/screens/HomeScreen/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import ProductItem from '../../components/ProductItem'
+import products from '../../data/products'
+
+
+const HomeScreen = () => {
+    return (
+        <View style={styles.page}>
+            {/* {Render Product Component} */}
+            <ProductItem item={products[1]}/>
+        </View>
+    );
+};
+const styles = StyleSheet.create({
+    page: {
+        padding: 10,
+    },
+});
+export default HomeScreen;
